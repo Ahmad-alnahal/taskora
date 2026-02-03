0bdbf1d812dae8071039403dad1a6612e7db6529
Ahmad alnahal <alnahal2003@gmail.com>
2026-02-03 13:29:34 +0200

    update Service Locator

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,14 +1,57 @@
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:taskora/features/auth/data/datasources/auth_local_datasource.dart';
+import 'package:taskora/features/auth/data/datasources/auth_remote_datasource.dart';
 import '../services/local/shared_pref_service.dart';
+import '../../features/auth/data/repositories/auth_repository_impl.dart';
+import '../../features/auth/domain/repositories/auth_repository.dart';
+import '../../features/auth/domain/usecases/login_usecase.dart';
+import '../../features/auth/domain/usecases/signup_usecase.dart';
+import '../../features/auth/domain/usecases/logout_usecase.dart';
+import '../../features/auth/domain/usecases/forgot_password_usecase.dart';
+import '../../features/auth/domain/usecases/verify_reset_code_usecase.dart';
+import '../../features/auth/domain/usecases/reset_password_usecase.dart';
+import '../../features/auth/domain/usecases/get_saved_token_usecase.dart';
+import '../../features/auth/presentation/bloc/auth_bloc.dart';
 
 final sl = GetIt.instance;
 
 Future<void> setupServiceLocator() async {
+  // External
   final prefs = await SharedPreferences.getInstance();
-
   sl.registerLazySingleton<SharedPreferences>(() => prefs);
-  sl.registerLazySingleton<SharedPrefService>(
-        () => SharedPrefService(sl()),
+
+  // Services
+  sl.registerLazySingleton<SharedPrefService>(() => SharedPrefService(sl()));
+
+  // DataSources
+  sl.registerLazySingleton<AuthLocalDataSource>(() => AuthLocalDataSource(sl()));
+  sl.registerLazySingleton<AuthRemoteDataSource>(() => AuthRemoteDataSource());
+
+  // Repository
+  sl.registerLazySingleton<AuthRepository>(
+        () => AuthRepositoryImpl(remote: sl(), local: sl()),
+  );
+
+  // UseCases
+  sl.registerLazySingleton(() => LoginUseCase(sl()));
+  sl.registerLazySingleton(() => SignupUseCase(sl()));
+  sl.registerLazySingleton(() => LogoutUseCase(sl()));
+  sl.registerLazySingleton(() => ForgotPasswordUseCase(sl()));
+  sl.registerLazySingleton(() => VerifyResetCodeUseCase(sl()));
+  sl.registerLazySingleton(() => ResetPasswordUseCase(sl()));
+  sl.registerLazySingleton(() => GetSavedTokenUseCase(sl()));
+
+  // Bloc
+  sl.registerFactory(
+        () => AuthBloc(
+      login: sl(),
+      signup: sl(),
+      logout: sl(),
+      forgotPassword: sl(),
+      verifyResetCode: sl(),
+      resetPassword: sl(),
+      getSavedTokenUseCase: sl(),
+    ),
   );
 }
