d9cc8ce005b066b203f07471ef0dc657c9b86d3a
Ahmad alnahal <alnahal2003@gmail.com>
2026-04-21 17:42:07 +0300

    save hourly rate

--- lib/features/auth/data/datasources/auth_local_datasource.dart
+++ lib/features/auth/data/datasources/auth_local_datasource.dart
@@ -4,8 +4,10 @@ class AuthLocalDataSource {
   AuthLocalDataSource(this.prefs);
   final SharedPreferences prefs;
 
-  static const _kTokenKey = 'auth_token';
+  static const _kTokenKey     = 'auth_token';
+  static const _kWatchCostKey = 'auth_watch_cost';
 
+  // Token
   Future<void> saveToken(String token) async {
     await prefs.setString(_kTokenKey, token);
   }
@@ -14,7 +16,18 @@ class AuthLocalDataSource {
     return prefs.getString(_kTokenKey);
   }
 
+  // hourly rate
+  Future<void> saveWatchCost(double watchCost) async {
+    await prefs.setDouble(_kWatchCostKey, watchCost);
+  }
+
+  double? getWatchCost() {
+    return prefs.getDouble(_kWatchCostKey);
+  }
+
+  //  Clear all (logout)
   Future<void> clearToken() async {
     await prefs.remove(_kTokenKey);
+    await prefs.remove(_kWatchCostKey);
   }
 }
